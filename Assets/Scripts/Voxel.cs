f58fb6788c9ab9be743aeeb8b0f4b6c89fb614bd
eunbyeol.kang <dsrfulp7@naver.com>
2025-09-15 10:52:05 +0900

    복셀이 뽀잉 튀어오르도록!

--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -7,11 +7,11 @@ public class Voxel : MonoBehaviour
     // 복셀이 날아갈 속도 속성
     public float speed = 5;
     public float destroyTime = 3.0f;  //복셀을 제거할 시간
-    float currentTime = 0;
-    void Start()
+    public float currentTime;
+    void OnEnable()
     {
-        Vector3 direction = Random.insideUnitSphere;  //크기가 1이고 방향만 존재함
-        
+        currentTime = 0;
+        Vector3 direction = Random.insideUnitSphere;  //크기가 1이고 방향만 존재함 
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
         rb.velocity = direction * speed;
     }
@@ -22,7 +22,9 @@ public class Voxel : MonoBehaviour
         currentTime = currentTime + Time.deltaTime; //시간이 흘러가도록
         if(currentTime > destroyTime)  //만약 현재 시간이 제거 시간을 초과했다면
         {
-            Destroy(gameObject); //게임 오브젝트를 제거해라
+            gameObject.SetActive(true); //자기 자신을 비할성화 함
+            VoxelMaker.voxelPool.Add(gameObject);  //VoxelMaker의 오브젝트 풀에 자기 자신을 추가
+            //Destroy(gameObject); //게임 오브젝트를 제거해라
         }
     }
 }
--- Assets/Scripts/VoxelMaker.cs
+++ Assets/Scripts/VoxelMaker.cs
@@ -6,10 +6,21 @@ public class VoxelMaker : MonoBehaviour
 {
     //생성할 대상 등록
     public GameObject voxelFactory;  //게임 오브젝트라는 변수가 만들어짐, public으로 등록했기 때문에 인스펙터에서 수정 가능
+    
+    //오브젝트 풀의 크기
+    public int voxelPoolSize = 20;
+
+    //오브젝트 풀, static은 내가 만든 변수는 오로지 1개라는 정적 함수
+    public static List<GameObject> voxelPool = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < voxelPoolSize; i++) //for문으로 20번 반복하게 하기
+        {
+            GameObject voxel = Instantiate(voxelFactory);  //시작하면 객체를 생성해라
+            voxel.SetActive(false); //복셀 비활성화
+            voxelPool.Add(voxel); //복셀을 오브젝트 풀에 담고 싶다
+        }
     }
 
     // Update is called once per frame
@@ -23,8 +34,14 @@ public class VoxelMaker : MonoBehaviour
             //마우스의 위치가 바닥 위에 위치한다면
             if(Physics.Raycast(ray, out hitInfo)) //ray에 대한 결과물이 true일 때 hitInfo에 넣어주는
             {
-                GameObject voxel = Instantiate(voxelFactory);  //복셀 공장에서 복셀을 만듦
-                voxel.transform.position = hitInfo.point;  //복셀을 배치
+                //복셀 오브젝트 풀 사용하기
+                if(voxelPool.Count > 0) //오브젝트 풀 안에 voxel이 있는지 확인
+                {
+                   GameObject voxel = voxelPool[0];  //오브젝트 풀에서 가장 처음 복셀을 하나 가져옴
+                   voxel.SetActive(true); //비활성화 되어있던 복셀을 활성화시킴
+                   voxel.transform.position = hitInfo.point;  //복셀을 배치
+                   voxelPool.RemoveAt(0); //오브젝트 풀에서 복셀을 제거함
+                }
             }
         }
     }
